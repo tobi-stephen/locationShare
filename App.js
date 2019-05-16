c3bae913b526a82b926ddc8f3becf21591d5d460
tobi_ade <tobi@grit.systems>
2019-05-16 20:45:49 +0100

    geofence creation and user location check

--- App.js
+++ App.js
@@ -7,22 +7,17 @@ import {
     TouchableOpacity,
     PermissionsAndroid,
 } from 'react-native';
-import {
-    Container,
-    Content,
-    Card,
-} from 'native-base';
 import Geolocation from 'react-native-geolocation-service';
-import MapView, { Marker, ProviderPropType } from 'react-native-maps';
+import GeoFencing from 'react-native-geo-fencing';
+import MapView, { Polygon, Marker, ProviderPropType } from 'react-native-maps';
 
 
 const { width, height } = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
-const LATITUDE = 37.78825;
-const LONGITUDE = -122.4324;
+const LATITUDE = 3.78825;
+const LONGITUDE = -6.4324;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-let id = 0;
 
 function randomColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
@@ -33,31 +28,93 @@ class DefaultMarkers extends React.Component {
         super(props);
 
         this.state = {
-            region: [],
-            regionP1: {
-                latitude: LATITUDE + 0.5,
-                longitude: LONGITUDE - 0.2,
+            geofence: false,
+            track: {
+                latitude: LATITUDE,
+                longitude: LONGITUDE,
                 latitudeDelta: LATITUDE_DELTA,
                 longitudeDelta: LONGITUDE_DELTA,
             },
-            markers: [],
+            coordinate: {
+                latitude: 37.8025259, longitude: -122.4351431
+            },
+            polygon: [
+                { lat: 3.1336599385978805, lng: 6.31866455078125 },
+                { lat: 3.3091633559540123, lng: 6.66198730468757 },
+                { lat: 3.091150714460597,  lng: 6.92977905273438 },
+                { lat: 2.7222113428196213, lng: 6.74850463867188 },
+                { lat: 2.7153526167685347, lng: 6.47933959960938 },
+                { lat: 3.1336599385978805, lng: 6.31866455078125 }
+            ],
+            coordinates: [
+                { latitude: 3.1336599385978805, longitude: 6.31866455078125 },
+                { latitude: 3.3091633559540123, longitude: 6.66198730468757 },
+                { latitude: 3.1336599385978805, longitude: 6.31866455078125 }
+            ]
         };
     }
 
     onMapPress(e) {
         this.setState({
-            markers: [
-                ...this.state.markers,
-                {
-                    coordinate: e.nativeEvent.coordinate,
-                    key: id++,
-                    color: randomColor(),
-                },
-            ],
-        });
+            coordinate: e.nativeEvent.coordinate
+        })
     }
 
-    myLocation = async (val) => {
+    checkUser = async () => {
+
+        if (!this.state.geofence){
+            alert("set geofence first")
+            return
+        }
+
+        let polygon = this.state.polygon
+        let point = this.state.point
+                        
+        GeoFencing.containsLocation(point, polygon)
+        .then(() => alert('user is within geofence'))
+        .catch(() => alert('user is NOT within geofence'))
+    }
+
+    // checkUser = async () => {
+    //     try {
+    //         const granted = await PermissionsAndroid.request(
+    //             PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    //             {
+    //                 title: 'Query Location',
+    //                 message: 'Query a location',
+    //                 buttonNeutral: 'Ask Me Later',
+    //                 buttonNegative: 'Cancel',
+    //                 buttonPositive: 'OK',
+    //             },
+    //         );
+    //         if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+    //             Geolocation.getCurrentPosition(
+    //                 (position) => {
+
+    //                     let point = {
+    //                         lat: position.coords.latitude,
+    //                         lng: position.coords.longitude
+    //                     };
+
+    //                     let polygon = this.state.polygon
+                        
+    //                       GeoFencing.containsLocation(point, polygon)
+    //                       .then(() => alert('user is within geofence'))
+    //                       .catch(() => alert('user is NOT within geofence')) 
+                        
+    //                 },
+    //                 (error) => this.setState({ error: error.message }),
+    //             )
+    //         } else {
+    //             alert("denied")
+    //         }
+
+    //     } catch (err) {
+    //         alert(err.message)
+    //     }
+    // }
+
+    setGeofence = async () => {
         try {
             const granted = await PermissionsAndroid.request(
                 PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
@@ -72,36 +129,45 @@ class DefaultMarkers extends React.Component {
             if (granted === PermissionsAndroid.RESULTS.GRANTED) {
                 Geolocation.getCurrentPosition(
                     (position) => {
-                        let region = {
+                        let track = {
                             latitude: position.coords.latitude,
                             longitude: position.coords.longitude,
                             latitudeDelta: LATITUDE_DELTA,
                             longitudeDelta: LONGITUDE_DELTA,
                         }
-                        let regionP1 = {
-                            latitude: region.latitude + Math.random(),
-                            longitude: region.longitude - Math.random(),
-                            latitudeDelta: LATITUDE_DELTA,
-                            longitudeDelta: LONGITUDE_DELTA,
+
+                        let lat = position.coords.latitude
+                        let lng = position.coords.longitude
+                        const offset = 0.008
+                        const polygon = [
+                            { lat: lat - offset, lng: lng - offset },
+                            { lat: lat + offset, lng: lng - offset },
+                            { lat: lat + offset, lng: lng + offset },
+                            { lat: lat - offset, lng: lng + offset },
+                            { lat: lat - offset, lng: lng - offset } // last point has to be same as first point
+                        ];
+
+                        const coordinates = [
+                            { latitude: lat - offset, longitude: lng - offset },
+                            { latitude: lat + offset, longitude: lng - offset },
+                            { latitude: lat + offset, longitude: lng + offset },
+                            { latitude: lat - offset, longitude: lng + offset },
+                            { latitude: lat - offset, longitude: lng - offset } // last point has to be same as first point
+                        ];
+
+                        let coordinate = {
+                            latitude: position.coords.latitude,
+                            longitude: position.coords.longitude
                         }
-                        // let apiKey = "AIzaSyBAefhRlXEH3vCko-zZTX6PHllTR6av4WI"
-                        // fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + region.latitude + ',' + region.longitude + '&key=' + apiKey)
-                        //     .then((resp) => resp.json())
-                        //     .then((respJson) => {
-                        //         alert(JSON.stringify(respJson))
-                        //     })
-                        let reg = this.state.region
-                        if (val == 0){
-                            reg[0] = region
-                        } else if (val == 1) {
-                            reg[1] = regionP1
-                        } else {
-                            reg[2] = regionP1
-                        }
-                        this.setState({ region: reg })
+
+                        let point = {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        };
+                        this.setState({ geofence: true, track, coordinates, polygon })
                     },
                     (error) => this.setState({ error: error.message }),
-                    // { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+                    { enableHighAccuracy: true },
                 )
             } else {
                 alert("denied")
@@ -112,122 +178,109 @@ class DefaultMarkers extends React.Component {
         }
     }
 
-    componentDidMount() {
+    async componentDidMount() {
+        try {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                {
+                    title: 'Query Location',
+                    message: 'Query a location',
+                    buttonNeutral: 'Ask Me Later',
+                    buttonNegative: 'Cancel',
+                    buttonPositive: 'OK',
+                },
+            );
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                this.watchId = Geolocation.watchPosition(
+                    (position) => {
+                        let track = {
+                            latitude: position.coords.latitude,
+                            longitude: position.coords.longitude,
+                            latitudeDelta: LATITUDE_DELTA,
+                            longitudeDelta: LONGITUDE_DELTA,
+                        }
 
+                        let offset = 0.008
+
+                        let point = {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        };
+
+                        let coordinate = {
+                            latitude: position.coords.latitude,
+                            longitude: position.coords.longitude
+                        }
+
+                        this.setState({ track, coordinate, point })
+
+                        // GeoFencing.containsLocation(point, this.state.polygon)
+                        // .then(() => alert('user is within geofence'))
+                        // .catch(() => alert('user is NOT within geofence'))
+                    },
+                    (error) => this.setState({ error: error.message }),
+                    { enableHighAccuracy: true, interval: 10000, fastestInterval: 5000, distanceFilter: 1, },
+                )
+            } else {
+                alert("denied")
+            }
+
+        } catch (err) {
+            alert(err.message)
+        }
+    }
+
+    componentWillUnmount() {
+        Geolocation.clearWatch(this.watchId);
     }
 
     render() {
-
-
         return (
-            <Container styles={StyleSheet.absoluteFillObject}>
-                <Content styles={StyleSheet.absoluteFillObject}>
-                    <Card styles={styles.mapView}>
-                        <MapView
-                            provider={this.props.provider}
-                            style={styles.map}
-                            initialRegion={this.state.region[0]}
-                            region={this.state.region[0]}
-                            showsMyLocationButton={true}
-                            liteMode={false}
-                            mapType={'standard'}
-                            onPress={(e) => this.onMapPress(e)}
-                        >
-                            {this.state.markers.map(marker => (
-                                <Marker
-                                    key={marker.key}
-                                    coordinate={marker.coordinate}
-                                    pinColor={marker.color}
-                                />
-                            ))}
-                        </MapView>
-                        <View style={styles.buttonContainer}>
-                            <TouchableOpacity
-                                onPress={() => this.myLocation(0)}
-                                style={styles.bubble}
-                            >
-                                <Text>Find my location</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity
-                                onPress={() => alert(JSON.stringify(this.state.region[0]))}
-                                style={styles.bubble}
-                            >
-                                <Text>My LongLat</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </Card>
-                    <Card styles={StyleSheet.absoluteFillObject}>
-                        <MapView
-                            provider={this.props.provider}
-                            style={styles.map}
-                            initialRegion={this.state.region[1]}
-                            region={this.state.region[1]}
-                            showsMyLocationButton={true}
-                            liteMode={false}
-                            mapType={'standard'}
-                            onPress={(e) => this.onMapPress(e)}
-                        >
-                            {this.state.markers.map(marker => (
-                                <Marker
-                                    key={marker.key}
-                                    coordinate={marker.coordinate}
-                                    pinColor={marker.color}
-                                />
-                            ))}
-                        </MapView>
-                        <View style={styles.buttonContainer}>
-                            <TouchableOpacity
-                                onPress={() => this.myLocation(1)}
-                                style={styles.bubble}
-                            >
-                                <Text>Person1 Location</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity
-                                onPress={() => alert(JSON.stringify(this.state.region[1]))}
-                                style={styles.bubble}
-                            >
-                                <Text>Person1 LongLat</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </Card>
-                    <Card styles={StyleSheet.absoluteFillObject}>
-                        <MapView
-                            provider={this.props.provider}
-                            style={styles.map}
-                            initialRegion={this.state.region[2]}
-                            region={this.state.region[2]}
-                            showsMyLocationButton={true}
-                            liteMode={false}
-                            mapType={'standard'}
-                            onPress={(e) => this.onMapPress(e)}
-                        >
-                            {this.state.markers.map(marker => (
-                                <Marker
-                                    key={marker.key}
-                                    coordinate={marker.coordinate}
-                                    pinColor={marker.color}
-                                />
-                            ))}
-                        </MapView>
-                        <View style={styles.buttonContainer}>
-                            <TouchableOpacity
-                                onPress={() => this.myLocation(2)}
-                                style={styles.bubble}
-                            >
-                                <Text>Person2 Location</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity
-                                onPress={() => alert(JSON.stringify(this.state.region[2]))}
-                                style={styles.bubble}
-                            >
-                                <Text>Person2 LongLat</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </Card>
-                </Content>
-            </Container>
+            <View styles={styles.container}>
+                <MapView
+                    provider={this.props.provider}
+                    style={styles.map}
+                    region={this.state.track}
+                    showsMyLocationButton={true}
+                    onPress={(e) => {
+                        let position = e.nativeEvent.coordinate
+                        let point = {
+                            lat: position.latitude,
+                            lng: position.longitude
+                        };
+                        this.setState({point, coordinate: e.nativeEvent.coordinate})
+                    }}
+                >
+                    <Polygon
+                        coordinates={this.state.coordinates}
+                        strokeColor="#000"
+                        strokeWidth={6}
+                    />
+                    <Marker
+                        coordinate={this.state.coordinate}
+                        pinColor={randomColor()}
+                        title={'user0'}
+                    />
+                </MapView>
+                <View style={{ ...styles.buttonContainer, }}>
+                    <TouchableOpacity
+                        onPress={() => this.setGeofence()}
+                        style={styles.bubble}
+                    >
+                        <Text style={styles.button}>Set Geofence</Text>
+                    </TouchableOpacity>
 
-        );
+                    <TouchableOpacity
+                        onPress={() => this.checkUser()}
+                        style={styles.bubble}
+                    >
+                        <Text style={styles.button}>Check if user in geofence</Text>
+                    </TouchableOpacity>
+                </View>
+
+
+            </View>
+        )
     }
 }
 
@@ -242,9 +295,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     map: {
-        // ...StyleSheet.absoluteFillObject,
-        height: 200,
-        marginVertical: 20
+        ...StyleSheet.absoluteFillObject,
+        height: height
     },
     mapView: {
         borderBottomColor: 'red',
@@ -261,15 +313,19 @@ const styles = StyleSheet.create({
         alignItems: 'stretch',
     },
     button: {
-        width: 80,
+        // width: 80,
+        color: 'red',
         paddingHorizontal: 12,
         alignItems: 'center',
         marginHorizontal: 10,
     },
     buttonContainer: {
         flexDirection: 'row',
+        justifyContent: 'flex-end',
+
         marginVertical: 20,
         backgroundColor: 'transparent',
+
     },
 });
 
--- android/app/src/main/java/com/mapp/MainApplication.java
+++ android/app/src/main/java/com/mapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.mapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.surialabs.rn.geofencing.GeoFencingPackage;
 import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new GeoFencingPackage(),
             new RNFusedLocationPackage(),
             new MapsPackage(),
             new RNGestureHandlerPackage()
